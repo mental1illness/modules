d2d09abced9067e8356e2c08b833bef6e1f0672c
nam <124532637+mental1illness@users.noreply.github.com>
2026-01-11 13:56:01 +0700

    Create elements.lua

---
+++ elements.lua
@@ -0,0 +1,40 @@
+local Utility = {} do
+	function Utility:AddShadow(_Instance, _Properties)
+		if not _Instance then
+			return
+		end
+		_Properties = _Properties or {}
+		local Cfg = {
+			PixelX = _Properties.Pixel or 1;
+			PixelY = _Properties.Pixel or 1;
+			Color = _Properties.Color or Color3.fromRGB(0, 0, 0);
+		}
+		local Shadow = _Instance:Clone();
+		if _Instance:IsA("TextLabel") then
+			Shadow.ZIndex = _Instance.ZIndex - 1;
+			Shadow.Size = UDim2.new(1, 0, 1, 0);
+			Shadow.AnchorPoint = Vector2.new(0, 0);
+			Shadow.Position = UDim2.new(0, Cfg.PixelX, 0, Cfg.PixelY);
+			Shadow.TextColor3 = Cfg.Color;
+			Shadow.Text = _Instance.Text;
+			_Instance:GetPropertyChangedSignal("Text"):Connect(function()
+				Shadow.Text = _Instance.Text
+			end)
+			Shadow.Parent = _Instance
+		elseif _Instance:IsA("Frame") then
+			Shadow.ZIndex = _Instance.ZIndex - 1;
+			Shadow.Size = UDim2.new(1, 0, 1, 0);
+			Shadow.AnchorPoint = Vector2.new(0, 0);
+			Shadow.Position = UDim2.new(0, Cfg.PixelX, 0, Cfg.PixelY);
+			Shadow.BackgroundColor3 = Cfg.Color;
+			Shadow.Parent = _Instance
+		else
+			Shadow.ZIndex = _Instance.ZIndex - 1;
+			Shadow.Size = _Instance.Size + UDim2.new(0, 1, 0, 1);
+			Shadow.AnchorPoint = _Instance.AnchorPoint;
+			Shadow.Position = _Instance.Position + UDim2.new(0, Cfg.PixelX, 0, Cfg.PixelY);
+			Shadow.Parent = _Instance
+		end
+	end
+end
+return Utility
