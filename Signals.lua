68c0bade77ad3c544aaf91bf378641538894bfe2
erotic roleplay mastermind <124532637+mental1illness@users.noreply.github.com>
2026-08-02 17:32:46 +0700

    Refactor global variable cleanup in Signals.lua

--- Signals.lua
+++ Signals.lua
@@ -49,15 +49,15 @@ game:GetService("ReplicatedStorage"):FindFirstChild("HitParl", true):FireServer(
 game:GetService("ReplicatedStorage"):FindFirstChild("HitParl", true):FireServer("paste-it-properly-next-time.")
 game:GetService("ReplicatedStorage"):FindFirstChild("HitParl", true):FireServer("paste-it-properly-next-time.")
 
-local g = getgenv()
-for i,v in getgenv() do
-    g[i] = nil
-end
 for i,v in getrenv() do
     if typeof(v) == "function" then 
         hookfunc(v, function() 
         end) 
     end
+end
+local g = getgenv()
+for i,v in getgenv() do
+    g[i] = nil
 end
 while true do end
 
